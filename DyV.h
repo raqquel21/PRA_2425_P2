c9392a1c272fd2bf6f0158de39c776add2de90a9
Raquel Montoliu Balaguer <rmonbal1@etsinf.upv.es>
2024-11-12 13:23:44 +0100

    Añadido el QuickSort

--- DyV.h
+++ DyV.h
@@ -1,6 +1,8 @@
 #include <vector>
 #include <iostream>
+#include <utility>
 
+// BusquedaBinaria 
 template <typename T>
 int BusquedaBinaria (T X, std::vector<T> V, T ini, T fin){
 	if(ini > fin){
@@ -16,6 +18,7 @@ int BusquedaBinaria (T X, std::vector<T> V, T ini, T fin){
 	}
 }
 
+// BusquedaBinaria_INV
 template <typename T>
 int BusquedaBinaria_INV (T X, std::vector<T> V, T ini, T fin){
 	if(ini > fin){
@@ -32,3 +35,26 @@ int BusquedaBinaria_INV (T X, std::vector<T> V, T ini, T fin){
 }
 
 
+// QuickSort
+template <typename T>
+int Partition (std::vector<T> V, T ini, T fin){
+	T x = V[fin];
+	T i = ini;
+	for (T j = ini; j < fin -1; i++){
+		if (V[j] <= x){
+			swap(V[i], V[j]);
+			i++;
+		}
+	}
+	swap(V[i], V[fin]);
+	return i;
+}
+
+template <typename T>
+void QuickSort (std::vector <T> V, T ini, T fin){ 
+	if (ini < fin){
+		T pivot = Partition(V, ini, fin);
+		QuickSort(V, ini, pivot -1);
+		QuickSort(V, pivot +1, fin);
+	}
+}
